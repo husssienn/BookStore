da6c82ac353a20859dccac9dcaee4ad0835dbeac
Omar Mohamed <omar@omar-laptop.lan>
2023-05-06 11:01:38 +0300

    Say hello to Hussien

--- app/src/main/java/bookstore/App.java
+++ app/src/main/java/bookstore/App.java
@@ -5,7 +5,7 @@ package bookstore;
 
 public class App {
     public String getGreeting() {
-        return "Hello omar!";
+        return "Hello Hussien!";
     }
 
     public static void main(String[] args) {
