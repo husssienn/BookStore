1ef6c52a411ecf180e93636c42bb189a678c6999
Hussien Badawy <hussienbadawy2018@gmail.com>
2023-05-06 00:57:54 -0700

    say hello to omar

--- app/src/main/java/bookstore/App.java
+++ app/src/main/java/bookstore/App.java
@@ -5,7 +5,7 @@ package bookstore;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Hello omar!";
     }
 
     public static void main(String[] args) {
