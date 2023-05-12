04f620a25b9b1f0f38a7b47580230069e7b82d31
Omar Mohamed <mohamed.omar67492@gmail.com>
2023-05-12 06:24:13 +0300

    Don't use static methods for Store class

--- app/src/main/java/bookstore/App.java
+++ app/src/main/java/bookstore/App.java
@@ -1,20 +1,19 @@
 package bookstore;
 
-import java.util.Objects;
-
 public class App {
     public static void main(String[] args) {
-        Store.addBook("1", "book1");
-        Store.addBook("2", "book2");
-        Store.addBook("3", "book3");
-        Store.addBook("4", "book4");
-        Store.addBook("5", "book5");
-        Store.addBook("6", "book6");
+        var store = new Store();
+        store.addBook("1", "book1");
+        store.addBook("2", "book2");
+        store.addBook("3", "book3");
+        store.addBook("4", "book4");
+        store.addBook("5", "book5");
+        store.addBook("6", "book6");
 
-        var booksByTitle = Store.searchAllBooksByTitle("book3");
+        var booksByTitle = store.searchAllBooksByTitle("book3");
         if (booksByTitle != null) {
             booksByTitle.forEach(System.out::println);
-            Store.deleteBook(booksByTitle.get(0));
+            store.deleteBook(booksByTitle.get(0));
         }
     }
 }
--- app/src/main/java/bookstore/Store.java
+++ app/src/main/java/bookstore/Store.java
@@ -16,19 +16,250 @@ import java.util.StringTokenizer;
 
 
 public class Store {
-    // books indexed by their names
-    public static final ArrayList<Book> books = new ArrayList<>();
-    private static final String filePath = "./store.dat";
+    // books indexed by their titles
+    public final ArrayList<Book> books = new ArrayList<>();
+    private final String filePath;
 
-    static {
-        init();
+    /**
+     * Construct a store with the default file path for storing the data
+     */
+    public Store() {
+        this("./store.dat");
     }
 
-    private static void init() {
+    /**
+     * Construct a store with the file path specified to store data
+     * @param filePath The path to store the store data
+     */
+    public Store(String filePath) {
+        this.filePath = filePath;
+        loadStoreData();
+    }
+
+    /**
+     * Adds a book to the store if not already in
+     *
+     * @param book The book to add
+     * @return true if the book didn't exist before and was added, false otherwise
+     */
+    public boolean addBook(Book book) {
+        var allBooksBySameTitle = searchAllBookIndicesByTitle(book.getTitle());
+        if (allBooksBySameTitle != null) {
+            for (int idx : allBooksBySameTitle) {
+                if (books.get(idx).equals(book)) {
+                    return false;
+                }
+            }
+        }
+
+        // insertion sort
+        var firstBiggerIdx = 0;
+
+        for (; firstBiggerIdx < books.size(); ++firstBiggerIdx) {
+            if (books.get(firstBiggerIdx).getTitle().compareTo(book.getTitle()) > 0) break;
+        }
+
+        if (firstBiggerIdx == 0) {
+            books.add(0, book);
+        } else if (firstBiggerIdx == books.size()) {
+            books.add(firstBiggerIdx, book);
+        } else {
+            books.add(firstBiggerIdx - 1, book);
+        }
+
+        return true;
+    }
+
+    /**
+     * Adds a book with the provided information
+     *
+     * @param isbn     The ISBN of the book
+     * @param title    The title of the book
+     * @param category The category of the book
+     * @return true if the book didn't exist before and was added, false otherwise
+     */
+    public boolean addBook(String isbn, String title, String category) {
+        return addBook(new Book(isbn, title, category));
+    }
+
+    /**
+     * Adds a book with the provided information
+     *
+     * @param isbn  The ISBN of the book
+     * @param title The title of the book
+     * @return true if the book didn't exist before and was added, false otherwise
+     */
+    public boolean addBook(String isbn, String title) {
+        return addBook(new Book(isbn, title));
+    }
+
+    /**
+     * Search a book by title
+     *
+     * @param title The title to search for
+     * @return The book with the requested title
+     */
+    public Book searchBookByTitle(String title) {
+        var idx = searchBookIdxByTitle(title);
+        if (idx == -1) return null;
+        return books.get(idx);
+    }
+
+    /**
+     * Search all books matching the title
+     *
+     * @param title The title to search for
+     * @return A list of the books matching the requested title
+     */
+    public ArrayList<Book> searchAllBooksByTitle(String title) {
+        var bookArrayList = new ArrayList<Book>();
+        var bookIndices = searchAllBookIndicesByTitle(title);
+        if (bookIndices == null) return null;
+        bookIndices.forEach(idx -> bookArrayList.add(books.get(idx)));
+        return bookArrayList;
+    }
+
+
+    /**
+     * Deletes a book that have the same title & ISBN
+     *
+     * @param book The book to delete
+     * @return true if the book was found and deleted, false otherwise
+     */
+    public boolean deleteBook(Book book) {
+        var allBookIndicesBySameTitle = searchAllBookIndicesByTitle(book.getTitle());
+        if (allBookIndicesBySameTitle == null) return false;
+        for (int idx : allBookIndicesBySameTitle) {
+            if (books.get(idx).equals(book)) {
+                books.remove(idx);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Delete all books that match the title & ISBN
+     *
+     * @param book The book to delete
+     * @return true if any book found and deleted, false otherwise
+     */
+    public boolean deleteAllBooks(Book book) {
+        var res = false;
+        var allBookIndicesBySameTitle = searchAllBookIndicesByTitle(book.getTitle());
+        if (allBookIndicesBySameTitle == null) return false;
+        for (int idx : allBookIndicesBySameTitle) {
+            if (books.get(idx).equals(book)) {
+                books.remove(idx);
+                res = true;
+            }
+        }
+        return res;
+    }
+
+//    /**
+//     * Delete a book that only matches the title
+//     *
+//     * @param title The title to search for
+//     * @return true if the book was found and deleted, false otherwise
+//     */
+//    public boolean deleteBookByTitle(String title) {
+//        var bookIdx = searchBookIdxByTitle(title);
+//        if (bookIdx != -1) {
+//            books.remove(bookIdx);
+//            return true;
+//        }
+//        return false;
+//    }
+//
+//    /**
+//     * Delete all books having the provided title
+//     * @param title The title of the books to delete
+//     * @return true if any book matched and was deleted, false otherwise
+//     */
+//    public boolean deleteAllBooksByTitle(String title) {
+//        var res = false;
+//        var bookIndices = searchAllBookIndicesByTitle(title);
+//        if (bookIndices == null) return false;
+//        // delete from the end of the list
+//        Collections.reverse(bookIndices);
+//        for (int idx : bookIndices) {
+//            res = true;
+//            books.remove(idx);
+//        }
+//        return res;
+//    }
+
+    public ArrayList<Book> getAllBooks() {
+        return new ArrayList<>(books);
+    }
+
+    @Override
+    public String toString() {
+        return "Store{" + "books=" + books + '}';
+    }
+
+    /**
+     * Search a book by title
+     *
+     * @param title The title to search for
+     * @return The index of that book in the list
+     */
+    private int searchBookIdxByTitle(String title) {
+        if (books.size() == 0) return -1;
+        // binary search
+        for (int start = 0, end = books.size() - 1; start < end; ) {
+            var middle = (start + end) / 2;
+            var res = books.get(middle).getTitle().compareTo(title);
+            if (res == 0) {
+                return middle;
+            } else if (res > 0) {
+                end = middle - 1;
+            } else {
+                start = middle + 1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Search all books matching the title
+     *
+     * @param title The title to search for
+     * @return A list of all the indices having the same title
+     * @implNote The list returned is sorted in ascending order
+     */
+    private ArrayList<Integer> searchAllBookIndicesByTitle(String title) {
+        if (books.size() == 0) return null;
+        var indices = new ArrayList<Integer>();
+        // binary search
+        for (int start = 0, end = books.size() - 1; start <= end; ) {
+            var middle = (start + end) / 2;
+            var res = books.get(middle).getTitle().compareTo(title);
+            if (res == 0) {
+                var idx = middle;
+
+                while (--idx >= 0) {
+                    if (books.get(idx).getTitle().compareTo(title) != 0) break;
+                }
+
+                while (++idx < books.size() && Objects.equals(books.get(idx).getTitle(), title)) indices.add(idx);
+
+                return indices;
+            } else if (res > 0) {
+                end = middle - 1;
+            } else {
+                start = middle + 1;
+            }
+        }
+        return null;
+    }
+
+    private void loadStoreData() {
         var path = Paths.get(filePath);
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             try (var file = new FileOutputStream(filePath)) {
-                file.write(new Store().toString().getBytes());
+                file.write(this.toString().getBytes());
             } catch (IOException e) {
                 System.err.println("Failed to save store content to file");
             }
@@ -81,224 +312,5 @@ public class Store {
                 System.err.println("Failed to open store file to read data");
             }
         }
-    }
-
-    /**
-     * Adds a book to the store if not already in
-     *
-     * @param book The book to add
-     * @return true if the book didn't exist before and was added, false otherwise
-     */
-    public static boolean addBook(Book book) {
-        var allBooksBySameTitle = searchAllBookIndicesByTitle(book.getTitle());
-        if (allBooksBySameTitle != null) {
-            for (int idx : allBooksBySameTitle) {
-                if (books.get(idx).equals(book)) {
-                    return false;
-                }
-            }
-        }
-
-        // insertion sort
-        var firstBiggerIdx = 0;
-
-        for (; firstBiggerIdx < books.size(); ++firstBiggerIdx) {
-            if (books.get(firstBiggerIdx).getTitle().compareTo(book.getTitle()) > 0) break;
-        }
-
-        if (firstBiggerIdx == 0) {
-            books.add(0, book);
-        } else if (firstBiggerIdx == books.size()) {
-            books.add(firstBiggerIdx, book);
-        } else {
-            books.add(firstBiggerIdx - 1, book);
-        }
-
-        return true;
-    }
-
-    /**
-     * Adds a book with the provided information
-     *
-     * @param isbn     The ISBN of the book
-     * @param title    The title of the book
-     * @param category The category of the book
-     * @return true if the book didn't exist before and was added, false otherwise
-     */
-    public static boolean addBook(String isbn, String title, String category) {
-        return addBook(new Book(isbn, title, category));
-    }
-
-    /**
-     * Adds a book with the provided information
-     *
-     * @param isbn  The ISBN of the book
-     * @param title The title of the book
-     * @return true if the book didn't exist before and was added, false otherwise
-     */
-    public static boolean addBook(String isbn, String title) {
-        return addBook(new Book(isbn, title));
-    }
-
-    /**
-     * Search a book by title
-     *
-     * @param title The title to search for
-     * @return The book with the requested title
-     */
-    public static Book searchBookByTitle(String title) {
-        var idx = searchBookIdxByTitle(title);
-        if (idx == -1) return null;
-        return books.get(idx);
-    }
-
-    /**
-     * Search all books matching the title
-     *
-     * @param title The title to search for
-     * @return A list of the books matching the requested title
-     */
-    public static ArrayList<Book> searchAllBooksByTitle(String title) {
-        var bookArrayList = new ArrayList<Book>();
-        var bookIndices = searchAllBookIndicesByTitle(title);
-        if (bookIndices == null) return null;
-        bookIndices.forEach(idx -> bookArrayList.add(books.get(idx)));
-        return bookArrayList;
-    }
-
-
-    /**
-     * Deletes a book that have the same title & ISBN
-     *
-     * @param book The book to delete
-     * @return true if the book was found and deleted, false otherwise
-     */
-    public static boolean deleteBook(Book book) {
-        var allBookIndicesBySameTitle = searchAllBookIndicesByTitle(book.getTitle());
-        if (allBookIndicesBySameTitle == null) return false;
-        for (int idx : allBookIndicesBySameTitle) {
-            if (books.get(idx).equals(book)) {
-                books.remove(idx);
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /**
-     * Delete all books that match the title & ISBN
-     *
-     * @param book The book to delete
-     * @return true if any book found and deleted, false otherwise
-     */
-    public static boolean deleteAllBooks(Book book) {
-        var res = false;
-        var allBookIndicesBySameTitle = searchAllBookIndicesByTitle(book.getTitle());
-        if (allBookIndicesBySameTitle == null) return false;
-        for (int idx : allBookIndicesBySameTitle) {
-            if (books.get(idx).equals(book)) {
-                books.remove(idx);
-                res = true;
-            }
-        }
-        return res;
-    }
-
-//    /**
-//     * Delete a book that only matches the title
-//     *
-//     * @param title The title to search for
-//     * @return true if the book was found and deleted, false otherwise
-//     */
-//    public static boolean deleteBookByTitle(String title) {
-//        var bookIdx = searchBookIdxByTitle(title);
-//        if (bookIdx != -1) {
-//            books.remove(bookIdx);
-//            return true;
-//        }
-//        return false;
-//    }
-//
-//    /**
-//     * Delete all books having the provided title
-//     * @param title The title of the books to delete
-//     * @return true if any book matched and was deleted, false otherwise
-//     */
-//    public static boolean deleteAllBooksByTitle(String title) {
-//        var res = false;
-//        var bookIndices = searchAllBookIndicesByTitle(title);
-//        if (bookIndices == null) return false;
-//        // delete from the end of the list
-//        Collections.reverse(bookIndices);
-//        for (int idx : bookIndices) {
-//            res = true;
-//            books.remove(idx);
-//        }
-//        return res;
-//    }
-
-    public static ArrayList<Book> getAllBooks() {
-        return new ArrayList<>(books);
-    }
-
-    /**
-     * Search a book by title
-     *
-     * @param title The title to search for
-     * @return The index of that book in the list
-     */
-    private static int searchBookIdxByTitle(String title) {
-        if (books.size() == 0) return -1;
-        // binary search
-        for (int start = 0, end = books.size() - 1; start < end; ) {
-            var middle = (start + end) / 2;
-            var res = books.get(middle).getTitle().compareTo(title);
-            if (res == 0) {
-                return middle;
-            } else if (res > 0) {
-                end = middle - 1;
-            } else {
-                start = middle + 1;
-            }
-        }
-        return -1;
-    }
-
-    /**
-     * Search all books matching the title
-     *
-     * @param title The title to search for
-     * @return A list of all the indices having the same title
-     * @implNote The list returned is sorted in ascending order
-     */
-    private static ArrayList<Integer> searchAllBookIndicesByTitle(String title) {
-        if (books.size() == 0) return null;
-        var indices = new ArrayList<Integer>();
-        // binary search
-        for (int start = 0, end = books.size() - 1; start <= end; ) {
-            var middle = (start + end) / 2;
-            var res = books.get(middle).getTitle().compareTo(title);
-            if (res == 0) {
-                var idx = middle;
-
-                while (--idx >= 0) {
-                    if (books.get(idx).getTitle().compareTo(title) != 0) break;
-                }
-
-                while (++idx < books.size() && Objects.equals(books.get(idx).getTitle(), title)) indices.add(idx);
-
-                return indices;
-            } else if (res > 0) {
-                end = middle - 1;
-            } else {
-                start = middle + 1;
-            }
-        }
-        return null;
-    }
-
-    @Override
-    public String toString() {
-        return "Store{" + "books=" + books + '}';
     }
 }
